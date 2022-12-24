21e2bc9d8fe3a3a79dbdf989646c3c0ca52b22af
asean092000 <anthonynguyen092000@gmail.com>
2022-12-24 18:40:31 +0800

    Update install app.

--- action.sh
+++ action.sh
@@ -14,6 +14,8 @@ bc_action() {
     read -p "enter URL and token: "  URL
     curl -o actions-runner-osx-x64-2.300.0.tar.gz -L https://github.com/actions/runner/releases/download/v2.300.0/actions-runner-osx-x64-2.300.0.tar.gz
     echo "623275e630bf936047bfab4e588f92213bd4481a8234f0a639bbeec01e678060  actions-runner-osx-x64-2.300.0.tar.gz" | shasum -a 256 -c
-    tar xzf ./actions-runner-osx-x64-2.300.0.tar.gz
+    sudo tar xzf ./actions-runner-osx-x64-2.300.0.tar.gz
+    ./config.sh $URL
+    sudo ./svc.sh install && sudo ./svc.sh start && sudo ./svc.sh status
 }
 bc_action;
--- lempstack.sh
+++ lempstack.sh
@@ -344,6 +344,36 @@ EOF
 done
 }
 
+bc_action() {
+    read -p "enter Domain: "  DOMAIN
+    cd /var/www/$DOMAIN && sudo mkdir actions-runner && cd actions-runner
+    SOURCE=${BASH_SOURCE[0]}
+    while [ -L "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+        DIR=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
+        SOURCE=$(readlink "$SOURCE")
+        [[ $SOURCE != /* ]] && SOURCE=$DIR/$SOURCE # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+    done
+    DIR=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
+    sudo chmod -R 777 $DIR
+    read -p "enter URL and token: "  URL
+    curl -o actions-runner-osx-x64-2.300.0.tar.gz -L https://github.com/actions/runner/releases/download/v2.300.0/actions-runner-osx-x64-2.300.0.tar.gz
+    echo "623275e630bf936047bfab4e588f92213bd4481a8234f0a639bbeec01e678060  actions-runner-osx-x64-2.300.0.tar.gz" | shasum -a 256 -c
+    sudo tar xzf ./actions-runner-osx-x64-2.300.0.tar.gz
+    ./config.sh $URL
+    sudo ./svc.sh install && sudo ./svc.sh start && sudo ./svc.sh status
+    cd ../../../../
+}
+
+bc_install_app() {
+    read -p "enter number of app: " END
+    START=1
+    for i in $(seq $START $END)
+    do
+        echo "Creating app: $i";
+        bc_action
+    done
+}
+
 bc_reboot() {
     echo "yes/no"
     read -p "System needs to reboot : " END
@@ -383,7 +413,7 @@ bc_install() {
 
 # initialized the whole installation.
 bc_init() {
-    echo -e "Choose a number \n1: Install environment\n2: Install port\n3: Install ssl for new site\n4: exit"
+    echo -e "Choose a number \n1: Install environment\n2: Install port\n3: Install ssl for new site\n4: install sourecode\n5 exit"
     read -p "Enter number: " END
     if [ $END == 1 ]
     then
@@ -399,6 +429,9 @@ bc_init() {
     then
        bc_sub
     elif [ $END == 4 ]
+    then
+       bc_install_app
+    elif [ $END == 5 ]
     then
        exit
     else
