adb65750483442a98faed11c0174addce5c6ab07
asean092000 <anthonynguyen092000@gmail.com>
2023-01-06 11:50:49 +0800

    no message

--- createuser.sh
+++ createuser.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 bc_createUser() {
-    read -p "enter username: "  username && sudo echo "%$username ALL=(ALL) NOPASSWD:ALL" | sudo tee -a /etc/sudoers && sudo adduser $username && sudo usermod -aG sudo $username su - $username
+    read -p "enter username: "  username && sudo echo "%$username ALL=(ALL) NOPASSWD:ALL" | sudo tee -a /etc/sudoers && sudo adduser $username && sudo usermod -aG sudo $username && su - $username
 }
 bc_createUser;
